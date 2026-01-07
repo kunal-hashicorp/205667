85c44237b83ad2ae40c333bbc135c90e5811b4fc
kunal-hashicorp <kunal.singh@hashicorp.com>
2026-01-07 11:20:09 +0530

    Update main.tf

--- folder1/main.tf
+++ folder1/main.tf
@@ -1,10 +1,10 @@
-resource "null_resource" "example3" {
+resource "null_resource" "example1" {
   provisioner "local-exec" {
     command = "echo 'I am testing from feature branch test3'"
   }
 }
 
-resource "null_resource" "example_4" {
+resource "null_resource" "example_2" {
   provisioner "local-exec" {
     command = "echo 'This is the second null resource'"
   }
