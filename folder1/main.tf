615b37cf94e6b18139b0796f415d4e4ba8207e86
kunal-hashicorp <kunal.singh@hashicorp.com>
2026-01-07 14:42:25 +0530

    Update main.tf

--- folder1/main.tf
+++ folder1/main.tf
@@ -1,5 +1,5 @@
-resource "tfe_organization" "platform" {
+resource "tfe_organization" "ks-test" {
   name                                                    = "platform"
-  email                                                   = xyz@example.com
+  email                                                   = "xyz@example.com"
   send_passing_statuses_for_untriggered_speculative_plans = true
 }
