56fa7a7722e7237fbb55029f0763032121d9a60b
kunal-hashicorp <kunal.singh@hashicorp.com>
2026-01-07 09:33:27 +0530

    Add files via upload

---
+++ folder2/main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo 'I am testing from feature branch test3'"
+  }
+}
+
+resource "null_resource" "example_2" {
+  provisioner "local-exec" {
+    command = "echo 'This is the second null resource'"
+  }
+}
